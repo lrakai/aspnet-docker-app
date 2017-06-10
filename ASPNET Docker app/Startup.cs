1252834232b25aa68ac3dc4faf086d08724d07fc
Logan Rakai <lmrakai@gmail.com>
2017-06-10 14:09:47 +0800

    Add project files.

---
+++ ASPNET Docker app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNET_Docker_app.Startup))]
+namespace ASPNET_Docker_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
